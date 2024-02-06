521a2ea63532ff803d9d29c25eca7e1e54c51963
Gustav Moller <65606851+gustavmoller@users.noreply.github.com>
2024-02-06 23:00:44 +0000

    mario bros

---
+++ mario_bros/run.sh
@@ -0,0 +1,4 @@
+# No dependencies to install for a static HTML/CSS/JS website
+
+# Serve the static files using a simple HTTP server
+python3 -m http.server 8000
---
+++ mario_bros/scripts.js
@@ -0,0 +1,1 @@
+// Currently empty, but could be used for future enhancements such as form validation or interactive elements.
